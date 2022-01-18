b865a7e9a34c57d7b4db2e6b9ea24e5da2c15657
Yahya Gilany <yahya.gilany@live.com>
2022-01-18 15:05:29 -0500

    added script.js file, demostrated multiple String functionalities

---
+++ script.js
@@ -0,0 +1,32 @@
+console.log(`Hello World`);
+const variableName = "Hello World Hello World Hello World Hello \
+World Hello World Hello World Hello World Hello World Hello World\
+ Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World ";
+const variableName2 = 'this isn\'t a string';
+const variableName3 = `Hello Worldkjahskjdfhlkasdf
+akdsfhlkjahsdlkfjh
+asdfkjgahlsdkfhjlksjhdflkhgkj`;
+
+const username = "John";
+console.log(username.length)
+console.log(username.toUpperCase())
+console.log(username.toLowerCase())
+
+const testString = `${username} is ${username.length} characters long`;
+console.log(testString.split(` `));
+console.log(testString.split(` `).length);
+
+// Concatenation  
+const welcomeMessage = "Hello " + username + "!";
+const welcomeMessage2 = `Hello ${username}!`;
+
+console.log(welcomeMessage);
+console.log(welcomeMessage2);
+
+const age = 31;
+const message = `${username} is ${age} years old.`;
+
+console.log(typeof username)
+
+const dateOfBirth = "2019-01-01";
+const x = 12;
