ca47c3566dfa359c3d9599735a9771bfcec4e92a
Yahya Gilany <yahya.gilany@live.com>
2022-01-19 14:24:07 -0500

    cleaned up the script file

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
+// To print a message to the console:
 console.log(`Hello World`);
-const variableName = "Hello World Hello World Hello World Hello \
-World Hello World Hello World Hello World Hello World Hello World\
- Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World ";
-const variableName2 = 'this isn\'t a string';
-const variableName3 = `Hello Worldkjahskjdfhlkasdf
-akdsfhlkjahsdlkfjh
-asdfkjgahlsdkfhjlksjhdflkhgkj`;
 
-const user = {
+// To have a multi-line text.
+const variableName1 = "You can have a long string split over multiple lines in the code file. \
+However, it's still presented as a single paragraph on the webpage. \
+Lorem ipsum dolor sit amet, consectetur adipiscing elit.\
+Vestibulum euismod, nisl eget consectetur sagittis, nisl nunc consectetur nunc,\
+eget aliquet nisl nunc eget lorem.";
+
+// woes of using a single quotes for your strings
+const variableName2 = 'if you\'re using a single quote to wrap your string, and want to use a comma, you\'ll need to use the escape character.';
+
+// creating a json object (JavaScript Object Notation)
+const user1 = {
   username: `John`,
   age: 31,
   dateOfBirth: "2019-01-01",
@@ -20,25 +25,38 @@ const user2 = {
   dateOfBirth: "2019-01-01",
   isMarried: false,
 }
-
-const users = [user, user2];
-
-console.log(user.age)
+// to print the objects
+console.log(user1)
 console.log(user2)
 
+// create an array of objects
+const users = [user1, user2];
 console.log(users)
 
+// to acccess object properties
+console.log(user1.age)
+console.log(user1[`age`])
 
+
+// to loop over an array of object and print out the username property: method 1
 console.log(`traditional way`)
 for (let i = 0; i < users.length; i++) {
   console.log(users[i].username);
 }
 
+// to loop over an array of object and print out the username property: method 2
 console.log(`new way`)
-for (let user of users) {
+// using `of` will assign the value of the array item to the variable
+for (let user of users) { 
   console.log(user.username);
 }
 
+// using `in` will assign the index of the value of the array item to the variable
+for (let userIndex in users) {
+  console.log(users[userIndex].username);
+}
+
+// Another example
 const arrayOfNumbers = [4, 5, 6, 9, 10];
 for(let number of arrayOfNumbers) {
   console.log(number);
@@ -47,4 +65,3 @@ console.log(`---------`)
 for(let numberIndex in arrayOfNumbers) {
   console.log(arrayOfNumbers[numberIndex]);
 }
-
