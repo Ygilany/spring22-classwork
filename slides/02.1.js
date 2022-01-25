0a09dc2b8becc9e3899240e07d6632ec2170b870
Yahya Gilany <yahya.gilany@live.com>
2022-01-24 23:39:19 -0500

    making a commit

---
+++ slides/02.1.js
@@ -0,0 +1,8 @@
+
+// Document.getElementById(``);
+// getElementsByClassName() – select elements by one or more class names.
+// querySelector() – select elements by CSS selectors.
+
+
+// getElementsByName() – select elements by name.
+// getElementsByTagName() – select elements by a tag name.
