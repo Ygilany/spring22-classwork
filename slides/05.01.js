0c438a584f7ba3e6d0082dbd55b88b07de69b38d
Yahya Gilany <yahya.gilany@live.com>
2022-02-15 10:45:11 -0500

    rest of the lecture

--- slides/05.01.js
+++ slides/05.01.js
@@ -1,26 +1,25 @@
 // HTML element references
 const preElement =document.getElementById(`result`);
 
-const request = new XMLHttpRequest();
-
-function getMessages(callback){
-  request.onreadystatechange = function() {
-    if (request.readyState !== request.DONE){return;}
-    if(request.status !== 200){
-      console.log(`Error: ${request.status}`);
-      return;
-    }
-    callback(request)
-  }
-  request.open('GET', 'https://it3049c-chat-application.herokuapp.com/messages');
-  request.send();
+function getMessages() {
+  return fetch(`https://it3049c-chat-application.herokuapp.com/messages`)
+    .then(response => response.json())
 }
 
-function onMessagesReceived(request) {
-  const response = JSON.parse(request.responseText);
-  console.log(response[0].sender)
-  preElement.innerHTML = JSON.stringify(response);
+function setupPage() {
+  getMessages()
+    .then(function (response){
+      // If request is successful:
+      preElement.innerHTML = JSON.stringify(response);
+    })
+    .catch(function(error){
+      // If request is unsuccessful:
+      preElement.innerHTML = `
+        ${error}
+      `;
+      preElement.style.color = 'red';
+    })
 }
 
 // EXECUTION of Function
-getMessages(onMessagesReceived);
+setupPage();
