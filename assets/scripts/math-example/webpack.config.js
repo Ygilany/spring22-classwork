d3ee33047445065e03e40b5da5708e6db4c506f4
Yahya Gilany <yahya.gilany@live.com>
2022-03-29 13:46:53 -0400

    start

--- assets/scripts/math-example/webpack.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  entry: './assets/scripts/math-example/index.js',
-  mode: `development`,
-  output: {
-    filename: 'math.js',
-    path: `/Users/gilanyym/git/Teaching/IT3049C/spring22-classwork/dist`,
-  },
-};
--- assets/scripts/thumbWar/webpack.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  entry: './assets/scripts/thumbWar/index.js',
-  mode: `development`,
-  output: {
-    filename: 'thumbWar.js',
-    path: `/Users/gilanyym/git/Teaching/IT3049C/spring22-classwork/dist`,
-  },
-};
